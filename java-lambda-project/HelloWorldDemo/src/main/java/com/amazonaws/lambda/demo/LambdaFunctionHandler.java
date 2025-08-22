8eac1d448b87481b04d74870ce24a271e725cec8
ramu455 <102644678+ramu455@users.noreply.github.com>
2025-08-22 13:38:30 +0530

    Add files via upload

---
+++ java-lambda-project/HelloWorldDemo/src/main/java/com/amazonaws/lambda/demo/LambdaFunctionHandler.java
@@ -0,0 +1,18 @@
+package com.amazonaws.lambda.demo;
+
+import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.RequestHandler;
+
+public class LambdaFunctionHandler implements RequestHandler<Object, String> {
+
+    @Override
+    public String handleRequest(Object input, Context context) {
+        context.getLogger().log("Input: " + input);
+
+        // TODO: implement your handler
+        return "Hello World from CodeBuild!";
+    }
+
+}
+
+// jar / war
